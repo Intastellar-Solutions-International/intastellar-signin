b274abd1677ead0e5b5b47df3a0a166cd2cd80c2
Felix A. Schultz <felix.schultz@engapho.com>
2024-04-02 19:45:31 +0200

    Fix user name display in login page

--- login.js
+++ login.js
@@ -78,7 +78,7 @@ async function checkUserLogin(){
         const loginbtn = document.querySelector(".IntastellarSignin");
 
         const intastellarSignInInfo = document.querySelector(".intastellarSignIn-info");
-        intastellarSignInInfo.innerHTML = "Sign in as " + user?.name;
+        intastellarSignInInfo.innerHTML = "Sign in as " + user.name;
         loginbtn.innerHTML += "<img style='width: 30px; height=: 30px; margin-right: 5px; border-radius: 50%; object-fit: cover;' class='intastellar-userProfile' src='"+user.image+"'>";
     }).catch(e => {
         /* console.log(e); */
