593b052f98c3819d45219d9e9236758cc3f0264b
Felix A. Schultz <felix.schultz@engapho.com>
2024-08-05 17:40:58 +0200

    chore: Update login.js to use HTTPS for login URL

--- login.js
+++ login.js
@@ -125,8 +125,8 @@ function signin() {
                 new IntastellarSolutionsSDKError("Invalid token issuer");
                 return;
             }
-
-            window.location.href = window.location.protocol + "://" + document.querySelector("[data-login_uri]").getAttribute("data-login_uri") + query + "&token=" + t;
+            console.log(window.location.protocol);
+            window.location.href = "https://" + document.querySelector("[data-login_uri]").getAttribute("data-login_uri") + query + "&token=" + t;
         } else if (document.querySelector("[data-login_callback]") != null) {
             const fn = window[document.querySelector("[data-login_callback]").getAttribute("data-login_callback")];
             new IntastellarSolutionsSDKSuccess("We´ve successfully send user data for: " + JSON.parse(window.atob(t)).name);
