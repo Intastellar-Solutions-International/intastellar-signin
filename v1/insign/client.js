070f7957db4fcd9000bf7052001ce9cb0d9fc1a6
Felix A. Schultz <felix.schultz@engapho.com>
2024-09-01 15:42:21 +0200

    Refactor user data handling and logout functionality in client.js and login.js

--- v1/insign/client.js
+++ v1/insign/client.js
@@ -380,7 +380,6 @@ const Intastellar = {
             },
             logout() {
                 document.cookie = "inta_acc=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                window.location.reload();
             },
         }
     }
