dbc5bbe25b5fbca92862c53fbac3e33578915054
Felix A. Schultz <felix.schultz@engapho.com>
2024-11-15 14:13:18 +0100

    Fix: improve user display structure in Intastellar popup header for better organization

--- v1/login.js
+++ v1/login.js
@@ -402,12 +402,25 @@ const Intastellar = {
                         }
 
                         if (user) {
-                            document.querySelector(".intastellar-popup-header").innerHTML = `<img src="${user.image}" class="intastellar-popup-userProfile"><div class="intastellar-popup-header-info"><p class="intastellar-popup-userName">${user.name.first}</p> <p class="intastellar-popup-header-email">${user.email}</p></div>`;
+                            document.querySelector(".intastellar-popup-header").innerHTML = `
+                            <div class="intastellar-popup-user">
+                                <img src="${user.image}" class="intastellar-popup-userProfile">
+                                <div class="intastellar-popup-header-info">
+                                    <p class="intastellar-popup-userName">${user.name.first}</p>
+                                    <p class="intastellar-popup-header-email">${user.email}</p>
+                                </div>
+                            </div>`;
                             document.querySelector(".intastellar-popup-button").innerHTML = "Continue as " + user.name.first;
                         }
 
                         if (user2) {
-                            document.querySelector(".intastellar-popup-header").innerHTML += `<img src="${user2.image}" class="intastellar-popup-userProfile"><div class="intastellar-popup-header-info"><p class="intastellar-popup-userName">${user2.name.first}</p> <p class="intastellar-popup-header-email">${user2.email}</p></div>`;
+                            document.querySelector(".intastellar-popup-header").innerHTML += `
+                            <div class="intastellar-popup-user">
+                                <img src="${user2.image}" class="intastellar-popup-userProfile">
+                                <div class="intastellar-popup-header-info">
+                                <p class="intastellar-popup-userName">${user2.name.first}</p>
+                                <p class="intastellar-popup-header-email">${user2.email}</p>
+                            </div>`;
                         }
 
                         if (user && user2) {
