10eeb864c21c573982adf5849cc0728b1d803044
Felix A. Schultz <felix.schultz@engapho.com>
2024-11-16 14:07:36 +0100

    Fix: correct onclick attribute in Intastellar login popup for proper signin function call

--- v1/login.js
+++ v1/login.js
@@ -381,7 +381,7 @@ const Intastellar = {
                             }
 
                             intastellarPopupButton.innerHTML = "Continue as " + user[0].name.first;
-                            intastellarPopupButton.setAttribute("onclick", "signin('" + user[0].email + ", " + appName + "')");
+                            intastellarPopupButton.setAttribute("onclick", "signin('" + user[0].email + "', '" + appName + "')");
                         } else {
                             intastellarPopupButton.innerHTML = "Continue by choosing an account";
                             intastellarPopupButton.setAttribute("onclick", "signin(null, appName)");
